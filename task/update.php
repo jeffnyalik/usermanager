a64ca92c78a36f30e00d0e07a292c57628c1974a
jeff <jeffnyak@gmail.com>
2017-11-20 14:57:06 +0300

    user manager application

---
+++ task/update.php
@@ -0,0 +1,54 @@
+<?php
+include_once('./config/Config.php');
+include_once('./lib/Database.php');
+include_once('./inc/header.php');
+?>
+
+<?php 
+    $db = new Database();
+    $id = $_GET['id'];
+    $getid = "SELECT * FROM tasks WHERE id=$id";
+    $query_id = $db->Select($getid)->fetch_assoc();
+    // check the form
+    if(isset($_POST['submit']))
+    {
+        $firstname= mysqli_real_escape_string($db->link, $_POST['firstname']);
+        $lastname = mysqli_real_escape_string($db->link, $_POST['lastname']);
+        $phone =    mysqli_real_escape_string($db->link, $_POST['phone']);
+        if($firstname == '' || $lastname == '' || $phone == ''){
+            $errors = "<div class='alert alert-danger'> the fields can not be empty"."</div>";
+            echo  $errors;
+        }else{
+            // update the database table
+            $ql = "UPDATE tasks SET firstname ='$firstname', lastname='$lastname', phone='$phone' WHERE id=$id";
+            $update_id = $db->Update($sql);
+        }
+    }
+
+?>
+<div class="container">
+<h3 class="text-center">User Manager Application</h3>
+<hr>
+<span><a href="index.php" class="btn btn-success">Go Back</a></span>
+<div style="max-width:500px; margin: 0 auto;">
+    <form action="create.php" method="post">
+        <div class="form-group">
+            <label for="Firstname">Firstname</label>
+            <input type="text" name="firstname" value="<?php echo $query_id['firstname']; ?>" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="Firstname">Lastname</label>
+            <input type="text" name="lastname" value="<?php echo $query_id['lastname']; ?>" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="Firstname">Telephone</label>
+            <input type="number" name="phone" value="<?php echo $query_id['phone']; ?>" placeholder="numbers only" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="submit" name="submit" class="btn btn-primary" value="update">
+            <!-- <input type="reset" name="reset" class="btn btn-success" value="Cancel"> -->
+        </div>
+    </form>
+   
+</div>
+</div>
